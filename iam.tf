5c671f7a22d42a2a500dff9efcfd3c9c3eb91393
davidcallen <david.c.allen1971@gmail.com>
2022-04-11 12:44:11 +0100

    Add PassRole to IAM to allow use of rancher managed node role on rancher-provisioned nodes.

--- iam.tf
+++ iam.tf
@@ -30,6 +30,8 @@ resource "aws_iam_user_policy_attachment" "rancher-server" {
   user       = data.aws_iam_user.rancher-server.user_name
   policy_arn = aws_iam_policy.rancher-server.arn
 }
+# Note that we allow PassRole for rolenames "*-k8s-rancher-managed-nodes" so the rancher-provisioned managed
+# clusters are allow to set an instance profile on themselves.
 resource "aws_iam_policy" "rancher-server" {
   name        = "${var.environment.resource_name_prefix}-rancher-server"
   description = "Allow Rancher server to manipulate AWS resources to create new clusters, add nodes etc..."
@@ -67,7 +69,8 @@ resource "aws_iam_policy" "rancher-server" {
                 "arn:aws:ec2:${var.aws_region}:${var.environment.account_id}:subnet/*",
                 "arn:aws:ec2:${var.aws_region}:${var.environment.account_id}:key-pair/*",
                 "arn:aws:ec2:${var.aws_region}:${var.environment.account_id}:network-interface/*",
-                "arn:aws:ec2:${var.aws_region}:${var.environment.account_id}:security-group/*"
+                "arn:aws:ec2:${var.aws_region}:${var.environment.account_id}:security-group/*",
+                "arn:aws:iam::${var.environment.account_id}:role/${var.environment.resource_name_prefix}-*-k8s-rancher-managed-nodes"
             ]
         },
         {
@@ -93,64 +96,6 @@ resource "aws_iam_policy" "rancher-server" {
 }
 EOF
 }
-
-//# Policy With PassRole  (needed if you want to use Kubernetes Cloud Provider or want to pass an IAM Profile to an instance)
-//resource "aws_iam_policy" "rancher-server" {
-//    name        = "${var.environment.resource_name_prefix}-rancher-server"
-//    description = "Allow Rancher server to manipulate AWS resources to create new clusters, add nodes etc..."
-//    policy      = <<EOF
-//{
-//    "Version": "2012-10-17",
-//    "Statement": [
-//        {
-//            "Sid": "EC2keyPairsAllow",
-//            "Effect": "Allow",
-//            "Action": [
-//                "ec2:AuthorizeSecurityGroupIngress",
-//                "ec2:Describe*",
-//                "ec2:ImportKeyPair",
-//                "ec2:CreateKeyPair",
-//                "ec2:CreateSecurityGroup",
-//                "ec2:CreateTags",
-//                "ec2:DeleteKeyPair",
-//                "ec2:ModifyInstanceMetadataOptions"
-//            ],
-//            "Resource": "*"
-//        },
-//        {
-//            "Sid": "EC2passRoleAllow",
-//            "Effect": "Allow",
-//            "Action": [
-//                "iam:PassRole",
-//                "ec2:RunInstances"
-//            ],
-//            "Resource": [
-//                "arn:aws:ec2:${var.aws_region}::image/ami-*",
-//                "arn:aws:ec2:${var.aws_region}:${var.environment.account_id}:instance/*",
-//                "arn:aws:ec2:${var.aws_region}:${var.environment.account_id}:placement-group/*",
-//                "arn:aws:ec2:${var.aws_region}:${var.environment.account_id}:volume/*",
-//                "arn:aws:ec2:${var.aws_region}:${var.environment.account_id}:subnet/*",
-//                "arn:aws:ec2:${var.aws_region}:${var.environment.account_id}:key-pair/*",
-//                "arn:aws:ec2:${var.aws_region}:${var.environment.account_id}:network-interface/*",
-//                "arn:aws:ec2:${var.aws_region}:${var.environment.account_id}:security-group/*",
-//                "arn:aws:iam::${var.environment.account_id}:role/${aws_iam_role.rancher-server.name}"
-//            ]
-//        },
-//        {
-//            "Sid": "EC2startStopAllow",
-//            "Effect": "Allow",
-//            "Action": [
-//                "ec2:RebootInstances",
-//                "ec2:TerminateInstances",
-//                "ec2:StartInstances",
-//                "ec2:StopInstances"
-//            ],
-//            "Resource": "arn:aws:ec2:${var.aws_region}:${var.environment.account_id}:instance/*"
-//        }
-//    ]
-//}
-//EOF
-//}
 
 # IAM Policy if want to allow encrypted EBS volumes
 //        {
